6b44f26623dfa4f66ecaeb36a267436edcf3797c
Eaziwest <israeleazi2876@gmail.com>
2024-08-04 12:51:17 +0100

    commit

--- components/StairEff.jsx
+++ components/StairEff.jsx
@@ -1,8 +1,9 @@
+// src/StairEff.jsx
 "use client";
 
 import { AnimatePresence, motion, easeInOut } from "framer-motion";
 import { usePathname } from "next/navigation";
-import Stairs from "./Stairs"; // Adjust the import path if needed
+import Stair from "./Initials"; // Adjust the import path if needed
 import "./StairEff.css"; // Import the CSS file
 
 const StairEff = () => {
@@ -10,23 +11,25 @@ const StairEff = () => {
     console.log("Current pathname:", pathname); // Debugging line
 
     return (
-        <>
-            <AnimatePresence mode="wait">
-                <div key={pathname}>
-                    <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
-                        <Stairs />
-                    </div>
-                    <motion.div 
-                        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none" 
-                        initial={{ opacity: 1 }} 
-                        animate={{ 
-                            opacity: 0, 
-                            transition: { delay: 2, duration: 0.4, ease: easeInOut },
-                        }}
-                    />
-                </div>
-            </AnimatePresence>
-        </>
+        <AnimatePresence>
+            <motion.div
+                key={pathname}
+                className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex items-center justify-center"
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { duration: 1 } }} // Add transition for fade-out
+            >
+                <Stair />
+            </motion.div>
+            <motion.div 
+                className="h-screen w-screen fixed bg-primary top-0 pointer-events-none" 
+                initial={{ opacity: 1 }} 
+                animate={{ 
+                    opacity: 0, 
+                    transition: { delay: 2, duration: 0.4, ease: easeInOut },
+                }}
+            />
+        </AnimatePresence>
     );
 };
 
